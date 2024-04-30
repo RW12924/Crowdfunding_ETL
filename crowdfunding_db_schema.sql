963e8e3a4eb52b7a7a0485d49f583316146a572d
RW12924 <rswong@comcast.net>
2024-04-29 21:19:42 -0700

    Final Submission

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE category_table (
+    category_id VARCHAR(30) PRIMARY KEY,
+    category VARCHAR(30)
+);
+CREATE TABLE subcategory_table (
+    subcategory_id VARCHAR(30) PRIMARY KEY,
+    subcategory VARCHAR(30)
+);
+CREATE TABLE contact_table (
+    contact_id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    email VARCHAR(50)
+);
+CREATE TABLE campaign_table (
+    cf_id VARCHAR(30) PRIMARY KEY,
+    company_name VARCHAR(30),
+    description VARCHAR(30),
+    goal FLOAT,
+    pledged FLOAT,
+    outcome VARCHAR(30),
+    backers_count INT,
+    country VARCHAR(30),
+    currency VARCHAR(30),
+    launched_date DATE,
+    end_date DATE,
+	contact_id INT,
+	category_id VARCHAR(30),
+	subcategory_id VARCHAR(30),
+	FOREIGN KEY (contact_id) REFERENCES contact_table(contact_id),
+    FOREIGN KEY (category_id) REFERENCES category_table(category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory_table(subcategory_id)
+);
